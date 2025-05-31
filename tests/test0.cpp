95ba57459d5dcb2df84477deed77fb376be41703
lockeystorm <wallie4@ya.ru>
2025-05-31 18:26:28 +0300

    Fixed test0 using mock-objects

--- tests/test0.cpp
+++ tests/test0.cpp
@@ -6,10 +6,10 @@
 class MockAccount : public Account { // gmoke нужно только для класса Account
 public:
     MockAccount(int id, int balance) : Account(id, balance) {}
-    MOCK_METHOD(int, GetBalance, (), (const));
-    MOCK_METHOD(void, ChangeBalance, (int));
-    MOCK_METHOD(void, Lock, (), ());
-    MOCK_METHOD(void, Unlock, (), ());
+    MOCK_METHOD(int, GetBalance, (), (const, override));
+    MOCK_METHOD(void, ChangeBalance, (int), (override));
+    MOCK_METHOD(void, Lock, (), (override));
+    MOCK_METHOD(void, Unlock, (), (override));
 };
 
 TEST(Account, ChangeBalanceWithoutLock) {
@@ -47,10 +47,45 @@ TEST(Transaction, SetFee){
     EXPECT_EQ(t.fee(), 12);
 }
 
+TEST(Transaction, TransactionCallsAccountMethodsCorrectly){
+    Transaction t;
+    MockAccount acc1(0, 2201);
+    MockAccount acc2(1, 1800);
+    
+    EXPECT_CALL(acc1, Lock()).Times(1);
+    EXPECT_CALL(acc2, Lock()).Times(1);
+    EXPECT_CALL(acc1, GetBalance()).WillOnce(::testing::Return(2201));
+    EXPECT_CALL(acc1, ChangeBalance(-201)).Times(1);
+    EXPECT_CALL(acc2, ChangeBalance(200)).Times(1);
+    EXPECT_CALL(acc1, Unlock()).Times(1);
+    EXPECT_CALL(acc2, Unlock()).Times(1);
+
+    EXPECT_TRUE(t.Make(acc1, acc2, 200));
+}
+
+TEST(Transaction, NotEnoughFunds){
+    Transaction t;
+    MockAccount acc1(0, 1000);
+    MockAccount acc2(1, 200);
+    
+    EXPECT_CALL(acc1, Lock()).Times(1);
+    EXPECT_CALL(acc2, Lock()).Times(1);
+    
+    EXPECT_CALL(acc2, ChangeBalance(2000)).Times(1);
+    EXPECT_CALL(acc1, GetBalance()).WillOnce(::testing::Return(1000));
+    EXPECT_CALL(acc1, ChangeBalance(-2001)).Times(0); // не должно быть вызова ChangeBalance, недостаточно средств
+    EXPECT_CALL(acc2, ChangeBalance(-2000)).Times(1);
+    
+    EXPECT_CALL(acc1, Unlock()).Times(1);
+    EXPECT_CALL(acc2, Unlock()).Times(1);
+    
+    EXPECT_FALSE(t.Make(acc1, acc2, 2000));
+}
+
 TEST(Transaction, IncorrectTransaction){
     Transaction t;
-    Account acc1(0, 1000);
-    Account acc2(1, 200);
+    MockAccount acc1(0, 1000);
+    MockAccount acc2(1, 200);
     
     EXPECT_THROW(t.Make(acc1, acc1, 150), std::logic_error);
     EXPECT_THROW(t.Make(acc1, acc2, -150), std::invalid_argument);
@@ -59,16 +94,6 @@ TEST(Transaction, IncorrectTransaction){
     t.set_fee(60);
     
     EXPECT_FALSE(t.Make(acc1, acc2, 115));
-    EXPECT_FALSE(t.Make(acc1, acc2, 2000));
 }
 
-TEST(Transaction, CorrectTransaction){
-    Transaction t;
-    Account acc1(0, 2201);
-    Account acc2(1, 1800);
-    
-    EXPECT_TRUE(t.Make(acc1, acc2, 200));
-    EXPECT_EQ(acc1.GetBalance(), 2000);
-    EXPECT_EQ(acc2.GetBalance(), 2000);
-}
 
